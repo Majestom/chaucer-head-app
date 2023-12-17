3472ff8e2fb0a1cceccac384f934c6e5dc8afcee
Tom <toalcoja@gmail.com>
2023-12-17 20:34:58 +0000

    Fix styling on text box.

--- src/app/components/FilterMenu/FilterMenu.css.ts
+++ src/app/components/FilterMenu/FilterMenu.css.ts
@@ -8,20 +8,21 @@ export const menu = style({
   height: 200,
   display: "flex",
   flexDirection: "column",
+  alignItems: "stretch",
+  justifyContent: "center",
   padding: 10,
   backgroundColor: "#97581F",
   boxShadow: "0px 0px 10px 8px rgba(0,0,0,0.75)",
 });
 
 export const textInput = style({
-  width: "100%",
   height: 40,
   padding: 10,
   backgroundColor: "#fff",
   color: "#000",
   borderRadius: 5,
   border: "none",
-  marginBottom: 10,
+  margin: 10,
   fontSize: 16,
 });
 
@@ -34,4 +35,5 @@ export const toggles = style({
   display: "flex",
   flexDirection: "column",
   gap: 10,
+  margin: 10,
 });
