b285aecfb84f5b680c61b28e889bd0c63998f6fc
Tom <toalcoja@gmail.com>
2024-01-07 14:24:39 +0000

    Add tanstack form.

--- src/app/components/AddCard/AddCard.tsx
+++ src/app/components/AddCard/AddCard.tsx
@@ -1,5 +1,44 @@
+import { useForm } from "@tanstack/react-form";
 import * as classes from "./AddCard.css";
 
 export default function AddCard() {
-  return <div className={classes.card}></div>;
+  const form = useForm({
+    defaultValues: {
+      fullName: "",
+    },
+    onSubmit: async ({ value }) => {
+      // Do something with form data
+      console.log(value);
+    },
+  });
+
+  return (
+    <div className={classes.card}>
+      <form.Provider>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            void form.handleSubmit();
+          }}
+        >
+          <div>
+            <form.Field name="fullName">
+              {(field) => (
+                <input
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) =>
+                    field.handleChange(e.target.value)
+                  }
+                />
+              )}
+            </form.Field>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </form.Provider>
+    </div>
+  );
 }
