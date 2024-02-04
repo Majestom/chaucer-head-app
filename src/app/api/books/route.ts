e3a4e38a204480b006aed6c5a4a9124b7cd320b1
Tom <toalcoja@gmail.com>
2024-02-04 22:52:12 +0000

    Add console log to results.

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   try {
     const result = await sql`SELECT * FROM books;`;
+    console.log("result:", result);
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
