9fe980ecef50a9da15be6eb268553aa80df9344c
Tom <toalcoja@gmail.com>
2024-02-05 21:42:54 +0000

    Add Strata to data.

--- src/app/components/Desktop/Desktop.tsx
+++ src/app/components/Desktop/Desktop.tsx
@@ -21,7 +21,7 @@ const BookSchema = z.object({
 
 const data = [
   {
-    id: 1,
+    id: "1",
     title: "Master Richard Quyny",
     author: "Fripp, Edgar I",
     description:
@@ -31,7 +31,7 @@ const data = [
     draft: true,
   },
   {
-    id: 2,
+    id: "2",
     title: "More About Worzel Gummidge",
     author: `Todd, Barbara Euphan ("Euphan")`,
     description: `Hardback, 8vo (13cm x 19cm) red cloth boards, black titles, first edition, 1st impression, , 138pp. Black and white illustrations. Good/NoWrapper. Gift Inscription. Binding tight, slight "lean". Spine faded, corners bumped, top and bottom of spine slightly bumped, small closed tear to bottom of spine. Binding tight. Boards and spine have slight staining and grubiness. Page block has slight discolouration and fading. Foxing to free front and back endpapers. Pages generally clean with the (very) occasional mark and a few corners are creased where previous readers have marked their place. As the title says, more about the classic scarecrow, Worzel Gummidge.`,
@@ -40,13 +40,22 @@ const data = [
     draft: false,
   },
   {
-    id: 3,
+    id: "3",
     title:
       "Rome and the Friendly King - The Character of Client Kingship",
     author: "Braund, David C.",
     description: `Hardback, 223mm x 137mm black cloth boards, gilt titles, first edition, 226pp. plus index, Very Good/Good +++. Book is tight and clean with no obvious imperfections. Dustwrapper is clean with minimal wear, just a little colour fading to spine, tanning to flaps. A keenly-researched account of how the Roman state worked with local and regional rulers during the late Republican and Imperial periods. Book is presented in a Courier font, right margin ragged. Catalogue: Greek / Roman culture.`,
     price: "£8",
     onSale: false,
+    draft: false,
+  },
+  {
+    id: "4",
+    title: "Strata",
+    author: "Terry Pratchett",
+    description: `Hardback, 8vo black cloth boards, silver titles, first edition, 1st impression, , 285pp. Good +/Good +. Book has been read, block edges have some dusting, spine has some creasing, pages clean. Dustwrapper (now protected) has some shelfwear, spine colour faded, some creasing to edges. A Discworld novel.`,
+    price: "£8",
+    onSale: true,
     draft: false,
   },
 ];
