a8697e7ca26130fd059c5d5726b4d007d57236cd
Tom <toalcoja@gmail.com>
2024-02-05 21:55:20 +0000

    Fix scroll issue on desktop.

--- src/app/components/Desktop/Desktop.css.ts
+++ src/app/components/Desktop/Desktop.css.ts
@@ -1,7 +1,8 @@
 import { style } from "@vanilla-extract/css";
 
 export const desktop = style({
-  height: "100vh",
+  height: "calc(100vh - 144px)",
+  overflowY: "auto",
   backgroundColor: "#085E10",
   padding: 20,
   display: "flex",
