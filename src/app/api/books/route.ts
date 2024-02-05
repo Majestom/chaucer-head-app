eab1f4aee66e2d428c8197c286eb30af9ac76866
Tom <toalcoja@gmail.com>
2024-02-05 21:16:49 +0000

    Comment out dynamic settings on GET books API.

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -1,8 +1,8 @@
 import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
+// export const dynamic = "force-dynamic";
+// export const revalidate = 0;
 
 export async function GET(request: Request) {
   try {
