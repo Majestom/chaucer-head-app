e85d143f3f3902218be47e448b9d60ce144dba19
Tom <toalcoja@gmail.com>
2024-03-02 21:55:49 +0000

    Remove tanstack react form.

--- src/app/components/AddCard/AddCard.tsx
+++ src/app/components/AddCard/AddCard.tsx
@@ -1,155 +1,64 @@
 import Image from "next/image";
 import { zodValidator } from "@tanstack/zod-form-adapter";
 import { z } from "zod";
-import { useForm } from "@tanstack/react-form";
 import { ToggleSwitch } from "../ToggleSwitch/ToggleSwitch";
 import { SaleIndicator } from "../SaleIndicator/SaleIndicator";
 import * as classes from "./AddCard.css";
 
 export default function AddCard() {
-  const form = useForm({
-    defaultValues: {
-      title: "",
-      author: "",
-      description: "",
-      price: 0,
-      onSale: false,
-    },
-    onSubmit: async ({ value }) => {
-      console.log("Form submitted!", value);
-      fetch(
-        "https://chaucer-head-app.vercel.app/api/add-book",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(value),
-        }
-      );
-      console.log(value);
-    },
-  });
-
   return (
     <div className={classes.card}>
-      <form.Provider>
-        <form
-          className={classes.form}
-          onSubmit={(e) => {
-            e.preventDefault();
-            e.stopPropagation();
-            void form.handleSubmit();
-          }}
-        >
-          <div className={classes.inputHolder}>
-            <form.Field name="title">
-              {(field) => (
-                <>
-                  <label htmlFor={field.name}>Title</label>
-                  <input
-                    className={classes.formField}
-                    name={field.name}
-                    value={field.state.value}
-                    onBlur={field.handleBlur}
-                    onChange={(e) =>
-                      field.handleChange(e.target.value)
-                    }
-                  />
-                </>
-              )}
-            </form.Field>
-            <form.Field name="author">
-              {(field) => (
-                <>
-                  <label htmlFor={field.name}>Author</label>
-                  <input
-                    className={classes.formField}
-                    name={field.name}
-                    value={field.state.value}
-                    onBlur={field.handleBlur}
-                    onChange={(e) =>
-                      field.handleChange(e.target.value)
-                    }
-                  />
-                </>
-              )}
-            </form.Field>
-            <form.Field name="description">
-              {(field) => (
-                <>
-                  <label htmlFor={field.name}>
-                    Description
-                  </label>
-                  <textarea
-                    className={classes.formField}
-                    name={field.name}
-                    value={field.state.value}
-                    onBlur={field.handleBlur}
-                    onChange={(e) =>
-                      field.handleChange(e.target.value)
-                    }
-                  />
-                </>
-              )}
-            </form.Field>
-            <form.Field
-              name="price"
-              validatorAdapter={zodValidator}
-              validators={{
-                onChange: z.number(),
-              }}
-            >
-              {(field) => (
-                <>
-                  <label htmlFor={field.name}>Price</label>
-                  <input
-                    className={classes.formField}
-                    name={field.name}
-                    value={field.state.value}
-                    onBlur={field.handleBlur}
-                    onChange={(e) =>
-                      field.handleChange(
-                        Number(e.target.value)
-                      )
-                    }
-                  />
-                </>
-              )}
-            </form.Field>
-            <form.Field name="onSale">
-              {(field) => (
-                <>
-                  <ToggleSwitch
-                    showLeft={field.state.value}
-                    setShowLeft={(e) =>
-                      field.handleChange(e.valueOf())
-                    }
-                    toggleLabel={"On Sale?"}
-                  />
-                  <SaleIndicator
-                    onSale={field.state.value}
-                  />
-                </>
-              )}
-            </form.Field>
-          </div>
-          <div className={classes.buttonHolder}>
-            <button
-              type="submit"
-              className={classes.button}
-            >
-              <Image
-                src={"/icons/add.svg"}
-                alt={"Add"}
-                width={50}
-                height={50}
-              />
-              Push To Submit
-            </button>
-          </div>
-        </form>
-      </form.Provider>
+      <form
+        className={classes.form}
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+        }}
+      >
+        <div className={classes.inputHolder}>
+          <label htmlFor={"title"}>Title</label>
+          <input
+            className={classes.formField}
+            name={"title"}
+          />
+
+          <label htmlFor={"author"}>Author</label>
+          <input
+            className={classes.formField}
+            name={"author"}
+          />
+
+          <label htmlFor={"description"}>Description</label>
+          <textarea
+            className={classes.formField}
+            name={"description"}
+          />
+
+          <label htmlFor={"price"}>Price</label>
+          <input
+            className={classes.formField}
+            name={"price"}
+          />
+
+          <ToggleSwitch
+            showLeft={true}
+            setShowLeft={() => true}
+            toggleLabel={"On Sale?"}
+          />
+          <SaleIndicator onSale={true} />
+        </div>
+        <div className={classes.buttonHolder}>
+          <button type="submit" className={classes.button}>
+            <Image
+              src={"/icons/add.svg"}
+              alt={"Add"}
+              width={50}
+              height={50}
+            />
+            Push To Submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
