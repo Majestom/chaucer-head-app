d70340874d3f12925f5e181a845bb5d9493d5f06
Tom <toalcoja@gmail.com>
2024-03-03 22:11:19 +0000

    Add react hook form.

--- src/app/components/AddCard/AddCard.tsx
+++ src/app/components/AddCard/AddCard.tsx
@@ -1,51 +1,95 @@
 import Image from "next/image";
-import { zodValidator } from "@tanstack/zod-form-adapter";
 import { z } from "zod";
+import { Controller, useForm } from "react-hook-form";
 import { ToggleSwitch } from "../ToggleSwitch/ToggleSwitch";
 import { SaleIndicator } from "../SaleIndicator/SaleIndicator";
 import * as classes from "./AddCard.css";
 
+const bookSchema = z.object({
+  title: z.string(),
+  author: z.string(),
+  description: z.string(),
+  price: z.number().multipleOf(0.01),
+  onSale: z.boolean(),
+});
+
+type Book = z.infer<typeof bookSchema>;
+
 export default function AddCard() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm<Book>();
+  console.log("errors: ", errors);
+  const onSubmit = (data: Book) => {
+    console.log(data);
+  };
   return (
     <div className={classes.card}>
       <form
         className={classes.form}
-        onSubmit={(e) => {
-          e.preventDefault();
-          e.stopPropagation();
-        }}
+        onSubmit={handleSubmit(onSubmit)}
       >
         <div className={classes.inputHolder}>
           <label htmlFor={"title"}>Title</label>
           <input
+            {...register("title", {
+              required: "Title is required.",
+            })}
+            placeholder="Title of the book"
             className={classes.formField}
             name={"title"}
           />
 
           <label htmlFor={"author"}>Author</label>
           <input
+            {...register("author", {
+              required: "Author is required.",
+            })}
+            placeholder="Author of the book"
             className={classes.formField}
             name={"author"}
           />
 
           <label htmlFor={"description"}>Description</label>
           <textarea
+            {...register("description", {
+              required: "Description is required.",
+            })}
+            placeholder="Description of the book"
             className={classes.formField}
             name={"description"}
           />
 
           <label htmlFor={"price"}>Price</label>
           <input
+            {...register("price", {
+              required: "Price is required.",
+            })}
+            placeholder="Price of the book"
             className={classes.formField}
             name={"price"}
           />
-
-          <ToggleSwitch
-            showLeft={true}
-            setShowLeft={() => true}
-            toggleLabel={"On Sale?"}
+          <Controller
+            control={control}
+            name="onSale"
+            render={({ field }) => (
+              <ToggleSwitch
+                showLeft={field.value}
+                setShowLeft={field.onChange}
+                toggleLabel={"On Sale?"}
+              />
+            )}
           />
-          <SaleIndicator onSale={true} />
+          <Controller
+            control={control}
+            name="onSale"
+            render={({ field }) => (
+              <SaleIndicator onSale={field.value} />
+            )}
+          />
         </div>
         <div className={classes.buttonHolder}>
           <button type="submit" className={classes.button}>
