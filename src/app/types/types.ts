02f5ced1d6a7a306cd312aa5726fc4e4cd7acc82
Tom <toalcoja@gmail.com>
2024-03-06 22:19:00 +0000

    Move types to separate file.

---
+++ src/app/types/types.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const BookSchema = z.object({
+  id: z.number(),
+  title: z.string(),
+  author: z.string(),
+  description: z.string(),
+  price: z.string().optional(),
+  image: z
+    .object({ data: z.number().array(), type: z.string() })
+    .optional(),
+  onSale: z.boolean().optional(),
+  draft: z.boolean().optional(),
+});
+
+export const BookDataSchema = z.array(BookSchema);
+
+export type BookDataType = z.infer<typeof BookDataSchema>;
