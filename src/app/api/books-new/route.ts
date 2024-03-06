e67944671141f9e23c2aab7aa608d10885ad1224
Tom <toalcoja@gmail.com>
2024-03-06 22:22:11 +0000

    Clean up GET books route.

--- src/app/api/books-new/route.ts
+++ src/app/api/books-new/route.ts
@@ -1,12 +1,11 @@
 import { NextResponse } from "next/server";
 import sqlite3 from "sqlite3";
 
-export async function GET(request: Request) {
-  // Define the path to your SQLite3 database file
+export async function GET() {
   const dbPath = "mydb.sqlite3";
 
-  // Create a new instance of the SQLite3 database
   const db = new sqlite3.Database(dbPath);
+
   return new Promise((resolve, reject) => {
     db.serialize(() => {
       db.all(`SELECT * FROM books;`, (err, rows) => {
@@ -16,7 +15,6 @@ export async function GET(request: Request) {
             NextResponse.json({ err }, { status: 500 })
           );
         } else {
-          console.log("rows:", rows);
           db.close();
           resolve(
             NextResponse.json({ rows }, { status: 200 })
