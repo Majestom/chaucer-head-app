fb468bfe822235abc8c732f18ef6680ce1333d66
Tom <toalcoja@gmail.com>
2024-04-08 22:17:00 +0100

    Update add book route.

--- src/app/api/add-book/route.ts
+++ src/app/api/add-book/route.ts
@@ -1,31 +1,35 @@
-// import { sql } from "@vercel/postgres";
 import sqlite3 from "sqlite3";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-  // const { searchParams } = new URL(request.url);
-  // const bookTitle = searchParams.get("title");
-  // const author = searchParams.get("author");
-  // console.log("request.json():", await request.json());
-  // console.log("body:", await request.body);
-
   const dbPath = "mydb.sqlite3";
 
   const db = new sqlite3.Database(dbPath);
 
-  const { title, author, description, onSale, price } =
-    await request.json();
-
-  console.log("title:", title);
-  console.log("author:", author);
-  console.log("description:", description);
+  const {
+    title,
+    author,
+    description,
+    onSale,
+    draft,
+    price,
+    image,
+  } = await request.json();
 
   return new Promise((resolve, reject) => {
     db.serialize(() => {
       db.all(
-        `INSERT INTO books (title, author, description) 
-      VALUES (?, ?, ?);`,
-        [title, author, description],
+        `INSERT INTO books (title, author, description, onSale, draft, price, image) 
+      VALUES (?, ?, ?, ?, ?, ?, ?);`,
+        [
+          title,
+          author,
+          description,
+          onSale,
+          draft,
+          price,
+          image,
+        ],
         (err, rows) => {
           if (err) {
             console.log("err:", err);
@@ -43,16 +47,4 @@ export async function POST(request: Request) {
       );
     });
   });
-
-  // try {
-  //   if (!title || !author)
-  //     throw new Error("Title and author required.");
-  //   const guid = uuidv4();
-  //   await sql`INSERT INTO books (Title, Author, Id, Description, Onsale, Draft, Coverimage) VALUES (${title}, ${author}, ${guid}, ${description}, ${true}, ${false}, ${"1.png"});`;
-  // } catch (error) {
-  //   return NextResponse.json({ error }, { status: 500 });
-  // }
-
-  // const books = await sql`SELECT * FROM books;`;
-  // return NextResponse.json({ books }, { status: 200 });
 }
